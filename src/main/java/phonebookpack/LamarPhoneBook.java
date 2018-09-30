79720ff49036c0237ae3e47ea870d4a2e6bbcd5f
lamarwitherspoon <43216093+lamarwitherspoon@users.noreply.github.com>
2018-09-30 19:31:56 -0400

    Update LamarPhoneBook.java

--- src/main/java/phonebookpack/LamarPhoneBook.java
+++ src/main/java/phonebookpack/LamarPhoneBook.java
@@ -5,4 +5,91 @@ public class LamarPhoneBook
     /*
     This is where Lamar will write his methods
      */
+
+
+
+
+
+
+public int lookup(char a){
+
+        this.lookup = char a;
+    }
+
+    public void  listNamaes(){
+
+        return listNamaes();
+ }
+
+
+
+    public lookupNumbers(String [] look){
+
+        this.lookupNumbers = look ;
+
+    }
+
+    public TreeMap<Integer, String> getTmap() {
+        return tmap;
+    }
+
+    public void removeRecord(String move){
+
+        this.removeRecord = move;
+        move.remove
+    }
+
+
+
+
+    public class PhoneBook {
+        //Zach and Zhodie  Everyone shoould copy this into your own respective class
+        // instance variable
+        private ArrayList<String> names;
+        private ArrayList<String> phonenumbers;
+        // Empty Constructor
+        public PhoneBook(){
+        }
+        //First method done for you i had some time tonight
+        public void add (String name,String phonenumber){
+            names.add(name);
+            phonenumbers.add(phonenumber);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
